cd10d9e1156d17e6ed74be68db329693b98be1eb
Vova <vova77gh@gmail.com>
2020-05-20 02:04:23 +0300

    Deploy. Change styles and API. Add static.json

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const proxy = createProxyMiddleware({
-  target: 'https://apiko-intensive-backend.herokuapp.com/',
+  target: 'https://marketplace-api-sv.herokuapp.com/',
   pathRewrite: {
     '^/api': '',
   },
